7e815f9b89978e069bf400c1740f0992900a5cbd
Ahdieh Emadi <ahdieh_e@yahoo.com>
2019-02-12 00:28:37 -0500

    Added some comments to code

--- GrayScaleConverter.java
+++ GrayScaleConverter.java
@@ -26,14 +26,20 @@ public class GrayScaleConverter {
         }
         // get image width and height
         int width = img.getWidth();
-        int height = img.getHeight();
+        int height = img.getHeight();               
         for(int y = 0; y < height; y++){
             for(int x = 0; x < width; x++){
+                // get the pixel of each x,y 
                 int p = img.getRGB(x,y);
+                // get the transparency(alpha)(a) that is the first byte (p>>24)
                 int a = (p>>24)&0xff;
+                // get the Red(r) that is the second byte (p>>16)
                 int r = (p>>16)&0xff;
+                // get the Green(g) that is the third byte (p>>8)
                 int g = (p>>8)&0xff;
+                // get the Blue(r) that is the forth byte (p)
                 int b = p&0xff;
+                // in gray scale red and green and blue are the same
                 int avg = (r+g+b)/3;
                 p = (a<<24) | (avg<<16) | (avg<<8) | avg;
                 img.setRGB(x, y, p);
