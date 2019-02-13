41d393020abe35727fcd8eaef456fa631c9d9326
Ahdieh Emadi <ahdieh_e@yahoo.com>
2019-02-12 23:19:02 -0500

    created a class to convert an image into gray scaleby using Duke libraries and wrote the algorithm

---
+++ GrayScaleConverterUsingDukeLibrary.java
@@ -0,0 +1,47 @@
+
+/**
+ * Write a description of GrayScaleConverterUsingDukeLibrary here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class GrayScaleConverterUsingDukeLibrary {
+    // starting with the inImage (the input image)
+    // make a blank image of the same size of inImage (the input image) and call it outImage
+
+        // for each pixel in outImage:
+        
+            // call the corresponding pixel in inImage, inPixel
+        
+            // Compute inPixel's red + inPixel's green + inPixel's blue
+            // divide the sum by 3 and call it average
+            
+            // set pixel's red to average
+            
+            // set pixel's green to average
+            
+            // set pixel's blue to average
+            
+        // outImage is the answer
+        
+        
+    // make a blank image of the same size of inImage (the input image) and call it outImage
+
+        // for each pixel in outImage:
+        
+            // call the corresponding pixel in inImage, inPixel
+        
+            // Compute inPixel's red + inPixel's green + inPixel's blue
+            // divide the sum by 3 and call it average
+            
+            // set pixel's red to average
+            
+            // set pixel's green to average
+            
+            // set pixel's blue to average
+            
+        // outImage is the answer
+        
+        
+}
